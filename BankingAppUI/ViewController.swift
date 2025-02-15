fd5061bbca0af83a914fe4db105953a1cbafaedb
Vaibhav <iosDeveloper1906@gmail.com>
2025-02-16 00:25:16 +0530

    Initial Commit

---
+++ BankingAppUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankingAppUI
+//
+//  Created by Vaibhav Gawde on 16/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BankingAppUIUITests/BankingAppUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BankingAppUIUITestsLaunchTests.swift
+//  BankingAppUIUITests
+//
+//  Created by Vaibhav Gawde on 16/02/25.
+//
+
+import XCTest
+
+final class BankingAppUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
